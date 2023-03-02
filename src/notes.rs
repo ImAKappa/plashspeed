d753c72857852c29beb9cc6e25908fdb827e4fe2
ImAKappa <imaninconsp1cuouskappa@gmail.com>
2023-03-01 23:39:07 -0500

    Update scaffold generation

--- src/notes.rs
+++ src/notes.rs
@@ -4,13 +4,70 @@ use std::io::{Write, Error};
 use log::{info};
 use jotdown::Parser;
 
-pub fn new(dir: String) -> Result<(), Error> {
-    info!("scaffold new notes at {}", dir);
-    let dir = Path::new(&dir);
+fn index_template() -> String {
+    let s = "# Welcome!
+
+PlashSpeed is a note-taking system that makes it easy to integrate flashcards into your notes.
+
+Flash cards are written like this:
+
+```
+:::card
+{.ft}
+This is the front of a card
+
+{.bk}
+This is the back of a card
+:::
+```
+
+And rendered into this:
+
+:::card
+{.ft}
+This is the front of a card
+
+{.bk}
+This is the back of a card
+:::
+
+See [plashspeed](https://github.com/ImAKappa/plashspeed) for examples and documentation.
+
+Plashspeed is built on top of Djot, a novel (and therefore experimental) markup language similar to MarkDown.
+See [Djot markup](https://djot.net/) for more details.
+";
+    String::from(s)
+}
+
+#[derive(Debug)]
+struct Config {
+    name: String,
+}
+
+fn serialize(c: Config) -> String {
+    format!("name=\"{}\"", c.name)
+}
+
+pub fn new(dirname: String) -> Result<(), Error> {
+    // Project Directory
+    let dir = Path::new(&dirname);
     create_dir_all(&dir)?;
-    let path = Path::new(&dir).join("index.dj");
+
+    // Config
+    let config = Config { name: String::from(&dirname) };
+    let path = dir.join("plash.toml");
     let mut output = File::create(path)?;
-    write!(output, "New notes!")?;
+    write!(output, "{}", serialize(config))?;
+
+    //Notes Directory
+    let notes_dir = dir.join("notes");
+    create_dir_all(&notes_dir)?;
+
+    // Example file
+    let path = notes_dir.join("index.dj");
+    let mut output = File::create(path)?;
+    write!(output, "{}", index_template())?;
+
     Ok(())
 }
 
